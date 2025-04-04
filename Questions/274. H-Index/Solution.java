53b9463573ef5699e6db7fdb848cffd181fb29c8
Alberto Vidana <alberto.vidana@gmail.com>
2025-04-03 23:57:48 -0600

    Complete of 3/4/2024 problem and H Index problem from 150 top list.

---
+++ Questions/274. H-Index/Solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        List<Integer> array = new ArrayList<Integer>();
+        array = citations;
+        return 0;
+    }
+}
--- top-interview-150/pro/JumpGame.java
+++ top-interview-150/pro/JumpGame.java
@@ -1,3 +1,5 @@
+package pro;
+
 /**
  * Array
 Greedy
