3fc159f273804dec234e007337b7de925102760b
Alberto Vidana <alberto.vidana@gmail.com>
2025-12-16 00:01:14 -0600

    #Update :: 2110. Number of Smooth Descent Periods of a Stock :: Adding solution code by LeetCode team

--- Daily Question/2110. Number of Smooth Descent Periods of a Stock/Solution.java
+++ Daily Question/2110. Number of Smooth Descent Periods of a Stock/Solution.java
@@ -1,3 +1,12 @@
-public class Solution {
-
+class Solution {
+    static public long getDescentPeriods(int[] prices) {
+        long sum=0, des=0;
+        int prev=-1;
+        for (int x : prices){
+            des=(x+1==prev?des:0)+1;
+            sum+=des;
+            prev=x;
+        }
+        return sum;
+    }
 }
