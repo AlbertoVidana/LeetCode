d65356cad310e84f9e2f23a6393b21203f8331b5
Alberto Vidana <alberto.vidana@gmail.com>
2026-03-15 20:43:46 -0600

    #Quest #DataBase :: Q4. User Activity for the Past 30 Days I :: Filtering & Aggregation Operation Cabin COMPLETED :: I complete this quest with a little or a lot of help by google but at the end it was my on solution #Easy #SQL #MySql #Database

--- Problems/Quest/DataBase/Filtering & Aggregation/Q3. Monthly Transactions I/File.sql
+++ Problems/Quest/DataBase/Filtering & Aggregation/Q3. Monthly Transactions I/File.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 SELECT 
-    trans_date AS month,
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
     country AS country,
-    COUNT(id) AS trans_count,
-    state AS approved_count,
-    amount AS trans_total_amount,
-    amount AS approved_total_amount
+    count(id) AS trans_count,
+    sum(state = 'approved') AS approved_count,
+    sum(amount) AS trans_total_amount,
+    sum(IF(state = 'approved', amount, 0)) AS approved_total_amount
 FROM Transactions
-GROUP BY country
+GROUP BY month, country
