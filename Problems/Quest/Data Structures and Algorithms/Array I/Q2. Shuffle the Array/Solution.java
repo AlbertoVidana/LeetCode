04534e07d2d7108260ca436e16785c6d238824c0
Alberto Vidana <alberto.vidana@gmail.com>
2026-03-24 17:36:43 -0600

    #Quest #DataStructure :: Q2. Shuffle the Array :: Solution by my own but it was a king of dificult even it was easy #Easy #Java #Time 00:15:00

---
+++ Problems/Quest/Data Structures and Algorithms/Array I/Q2. Shuffle the Array/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[n*2];
+        for(int i = 0, j = 1; i < n; i++) {
+            ans[i+i] = nums[i];
+            ans[i+i+1] = nums[i+n];
+        }
+        return ans;
+    }
+}
